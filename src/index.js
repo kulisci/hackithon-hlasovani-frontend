9aa44b1c92e8b5166589d2617e659d5ca4efdd59
Ondřej Hliba <hliba.ondra@live.com>
2019-05-04 00:45:47 +0200

    Add styles for styling html and body

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import './styles.css';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const jsx = <AppRouter />;
+
+ReactDOM.render(jsx, document.getElementById('root'));
